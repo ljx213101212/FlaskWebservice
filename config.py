fa42a955cd923ff9b25ffd004b3c95f049e8ba35
李继庠 <nusseres@gmail.com>
2015-09-17 09:35:10 +0800

    testing 2

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 DEBUG = True
 TESTING= True
 
-DATABASE = 'ljx2:root@173.194.246.202:3306/helloGoogle'
+DATABASE = 'ljx2:root@173.194.109.213:3306/helloGoogle'
 DATABASEURI = 'mysql+mysqldb://' + DATABASE
